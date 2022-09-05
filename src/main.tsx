752ef8eeba1567aecf73126769bb5cb40e1a1b52
Jose Negrin <soyjosenegrin@gmail.com>
2022-09-05 19:58:17 +0200

    updated to react v18

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 )
